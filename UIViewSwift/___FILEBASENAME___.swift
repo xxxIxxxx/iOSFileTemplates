c91a2ea8246edaf612d27152c8e582b3a5dfa77f
i2yf <>
2021-11-10 18:07:39 +0800

    去掉重复注释

--- UIViewSwift/___FILEBASENAME___.swift
+++ UIViewSwift/___FILEBASENAME___.swift
@@ -16,15 +16,6 @@ class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-
-  /*
-   // Only override draw() if you perform custom drawing.
-   // An empty implementation adversely affects performance during animation.
-   override func draw(_ rect: CGRect) {
-       // Drawing code
-   }
-   */
-
   /*
    // Only override draw() if you perform custom drawing.
    // An empty implementation adversely affects performance during animation.
